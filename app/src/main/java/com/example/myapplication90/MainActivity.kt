caa43d72f3b33c939b007c0b8c83e0d0ec606ef9
Radman Hayati <radman.hayatii@gmail.com>
2021-02-06 21:41:45 +0330

    time machine

--- app/src/main/java/com/example/myapplication90/MainActivity.kt
+++ app/src/main/java/com/example/myapplication90/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("new commit",null).show()
+                    .setAction("new action", null).show()
         }
     }
 
