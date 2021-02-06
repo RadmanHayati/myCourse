a2d8c337bbc09bdd3bf22e08b35c0b52f0cc26d0
Radman Hayati <radman.hayatii@gmail.com>
2021-02-06 19:26:33 +0330

    sth

--- app/src/main/java/com/example/myapplication90/MainActivity.kt
+++ app/src/main/java/com/example/myapplication90/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("this way", null).show()
+                    .setAction("that way", null).show()
         }
     }
 
